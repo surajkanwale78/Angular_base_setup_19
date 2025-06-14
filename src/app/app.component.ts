60577e9fcc97aa09a10142ec705ba891c6c85d81
surajkanwale78 <surajkanwale@gmail.com>
2025-06-14 12:55:57 +0530

    Intial Setup ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
