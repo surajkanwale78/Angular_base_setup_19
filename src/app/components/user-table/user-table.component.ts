096649a8439e54c8cd25bf451892ea4dd27bc3a4
surajkanwale78 <surajkanwale@gmail.com>
2025-06-14 20:41:14 +0530

    search functionlity with debounce

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-
+// import { PostService } from '../services/post.service';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css'
 })
 export class UserTableComponent {
+  posts: any[] = [];
+  filteredPosts: any[] = [];
+  loading = false;
+  searchSubject = new Subject<string>();
+  constructor(private userService: UserService) {}
+   ngOnInit() {
+    this.loading = true;
+    this.userService.getPosts().subscribe(data => {
+      this.posts = data;
+      this.filteredPosts = data;
+      this.loading = false;
+    });
 
+    this.searchSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe(searchText => {
+      this.filteredPosts = this.posts.filter(post =>
+        post.title.toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+  }
+
+  onSearch(value: string) {
+    this.searchSubject.next(value);
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class UserService {
+ private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
