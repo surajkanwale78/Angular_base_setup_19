8a4b53071175c37497afc4db3c282944398f3f25
surajkanwale78 <surajkanwale@gmail.com>
2025-06-14 21:49:58 +0530

    pagination and table added

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-table',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css'
 })
-export class UserTableComponent {
+export class UserTableComponent implements OnInit {
+  posts: any[] = [];
+  paginatedPosts: any[] = [];
+  loading = false;
+  itemsPerPage = 10;
+  currentPage = 1;
+  totalPages = 0;
 
-}
+  constructor(private userService : UserService) {}
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getPosts().subscribe(data => {
+      this.posts = data;
+      this.totalPages = Math.ceil(this.posts.length / this.itemsPerPage);
+      this.setPage(1);
+      this.loading = false;
+    });
+  }
+  setPage(page: number) {
+    this.currentPage = page;
+    const start = (page - 1) * this.itemsPerPage;
+    this.paginatedPosts = this.posts.slice(start, start + this.itemsPerPage);
+  }
+  }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
