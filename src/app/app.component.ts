48a631f96f9e53c103e2ebb838765375b6a5c90c
surajkanwale78 <surajkanwale@gmail.com>
2025-06-14 14:18:00 +0530

    standalone: true,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { UserTableComponent } from './components/user-table/user-table.component
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [UserTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
