12a3f49b4ed412e1b8f7c1c10196e1959b56c391
surajkanwale78 <surajkanwale@gmail.com>
2025-06-15 10:59:27 +0530

    reactive form addeed with validation

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 @Component({
   selector: 'app-user-table',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css'
 })
 export class UserTableComponent {
+ userForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', Validators.required)
+    },
+    { validators: [this.passwordMatchValidator] }
+  );
 
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form Submitted', this.userForm.value);
+    } else {
+      this.userForm.markAllAsTouched(); // Mark all fields to show validation
+    }
+  }
 }
