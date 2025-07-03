850a0b1ada1a71232471a64e6dd859bdc5f42128
surajkanwale78 <surajkanwale@gmail.com>
2025-07-03 16:20:53 +0530

    Added custom pipe

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe } from '../../pipe/currency.pipe';
 
 @Component({
   selector: 'app-user-table',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css'
 })
---
+++ src/app/pipe/currency.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currency'
+})
+export class CurrencyPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return `hello ${value}`;
+  }
+  
+}
