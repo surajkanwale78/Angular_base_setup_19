749f68f117afcb94de4288ce6b8eb21149d6f34e
surajkanwale78 <surajkanwale@gmail.com>
2025-07-03 15:42:25 +0530

    Added bulit in pipe

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.css'
 })
 export class UserTableComponent {
-
+title = "Suraj";
+today = new Date ();
+amount = 10;
 }
